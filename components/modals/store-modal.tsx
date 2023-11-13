67857de1757056690f31681c9e6251f087be3dfe
RicardoCS18 <ricardog.carreras@gmail.com>
2023-11-13 02:43:20 -0500

    Navbar creation

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,10 +1,13 @@
 "use client";
 import * as z from "zod";
+import { useState } from "react";
+import axios from "axios";
 
 import { Modal } from "@/components/ui/modal";
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { toast } from "react-hot-toast"
 import {
   Form
   , FormField
@@ -22,14 +25,27 @@ const formSchema = z.object({
 
 export const StoreModal = () => {
   const storeModal = useStoreModal();
+  const [loading, setLoading] = useState(false);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
     }
-  })
+  });
+
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    try {
+      setLoading(true);
+
+      const response = await axios.post('/api/stores', values)
+
+      toast.success("Store created.");
+    } catch (error) {
+      toast.error("Something went wrong.");
+    } finally {
+      setLoading(false);
+    }
   }
 
   return (
@@ -50,14 +66,20 @@ export const StoreModal = () => {
                   <FormItem>
                     <FormLabel>Name</FormLabel>
                     <FormControl>
-                      <Input placeholder="E-Commerce" {...field} />
+                      <Input
+                        disabled={loading}
+                        placeholder="E-Commerce" {...field} />
                     </FormControl>
                   </FormItem>
                 )}
               />
               <div className="pt-6 space-x-2 flex items-center justifty-end">
-                <Button variant="destructive" onClick={storeModal.onClose}>Cancel</Button>
-                <Button type="submit">Continue</Button>
+                <Button
+                  disabled={loading}
+                  variant="destructive" onClick={storeModal.onClose}>Cancel</Button>
+                <Button
+                  disabled={loading}
+                  type="submit">Continue</Button>
               </div>
             </form>
           </Form>
