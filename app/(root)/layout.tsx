4c627553eb6efa9498fb20dec8ccbd030633ba5a
RicardoCS18 <ricardog.carreras@gmail.com>
2023-11-15 13:05:43 -0500

    added billboard settings

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,23 +3,25 @@ import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation"
 
 export default async function SetupLayout({
-    children
+  children
 }: {
-    children: React.ReactNode
+  children: React.ReactNode
 }) {
-    const { userId } = auth();
-    if (!userId) redirect('/sign-in');
-    const store = await prismadb.store.findFirst({
-        where: {
-            userId
-        }
-    })
-    if (store) {
-        redirect(`/${store.id}`);
+
+  const { userId } = auth();
+  if (!userId) redirect('/sign-in');
+
+  const store = await prismadb.store.findFirst({
+    where: {
+      userId
     }
-    return (
-        <>
-            {children}
-        </>
-    )
+  })
+  if (store) {
+    redirect(`/${store.id}`);
+  }
+  return (
+    <>
+      {children}
+    </>
+  )
 }
