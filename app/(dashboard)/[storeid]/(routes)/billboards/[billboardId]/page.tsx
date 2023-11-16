6680ac46766b2cfbf3d96047c2efcc907d591d1a
RicardoCS18 <ricardog.carreras@gmail.com>
2023-11-15 23:30:56 -0500

    added billboard form

--- app/(dashboard)/[storeid]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeid]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,4 +1,5 @@
 import prismadb from "@/lib/prismadb"
+import BillboardForm from "./components/billboard-form"
 
 const BillboardPage = async ({
   params
@@ -13,7 +14,7 @@ const BillboardPage = async ({
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        {/* {Here goes BillboardForm} */}
+        <BillboardForm initialData={billboard} />
       </div>
     </div>
   )
