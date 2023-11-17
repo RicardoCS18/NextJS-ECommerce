31a8598b89ede5a9ec83a783194619093ca54f4b
RicardoCS18 <ricardog.carreras@gmail.com>
2023-11-17 09:27:23 -0500

    added categories

--- app/(dashboard)/[storeid]/(routes)/categories/[categoryId]/components/category-form.tsx
+++ app/(dashboard)/[storeid]/(routes)/categories/[categoryId]/components/category-form.tsx
@@ -1,7 +1,7 @@
 "use client"
 import axios from "axios"
 import * as z from "zod"
-import { Billboard } from "@prisma/client"
+import { Category } from "@prisma/client"
 import { Trash } from "lucide-react"
 import toast from "react-hot-toast"
 import { useParams, useRouter } from "next/navigation"
@@ -16,20 +16,19 @@ import { useState } from "react"
 import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { AlertModal } from "@/components/modals/alert-modal"
-import ImageUpload from "@/components/ui/image-upload"
 
 const formSchema = z.object({
-  label: z.string().min(1),
-  imageUrl: z.string().min(1)
+  name: z.string().min(1),
+  billboardId: z.string().min(1)
 });
 
-type BillboardFormValues = z.infer<typeof formSchema>
+type CategoryFormValues = z.infer<typeof formSchema>
 
-interface BillboardFormProps {
-  initialData: Billboard | null
+interface CategoryFormProps {
+  initialData: Category | null
 }
 
-const BillboardForm: React.FC<BillboardFormProps> = ({
+const CategoryForm: React.FC<CategoryFormProps> = ({
   initialData
 }) => {
 
@@ -39,20 +38,20 @@ const BillboardForm: React.FC<BillboardFormProps> = ({
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const title = initialData ? "Edit billboard" : "Create billboard";
-  const description = initialData ? "Edit billboard" : "Add a new billboard";
-  const toastMessage = initialData ? "Billboard updated." : "Billboard created";
+  const title = initialData ? "Edit category" : "Create category";
+  const description = initialData ? "Edit category" : "Add a new category";
+  const toastMessage = initialData ? "Category updated." : "Category created";
   const action = initialData ? "Save changes" : "Create"
 
-  const form = useForm<BillboardFormValues>({
+  const form = useForm<CategoryFormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: initialData || {
-      label: '',
-      imageUrl: ''
+      name: '',
+      billboardId: ''
     }
   });
 
-  const onSubmit = async (data: BillboardFormValues) => {
+  const onSubmit = async (data: CategoryFormValues) => {
     try {
       setLoading(true);
       if (initialData) {
@@ -113,32 +112,15 @@ const BillboardForm: React.FC<BillboardFormProps> = ({
           onSubmit={form.handleSubmit(onSubmit)}
           className="space-y-8 w-full"
         >
-          <FormField
-            control={form.control}
-            name="imageUrl"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel> Background image </FormLabel>
-                <FormControl>
-                  <ImageUpload
-                    value={field.value ? [field.value] : []}
-                    disabled={loading}
-                    onChange={(url) => field.onChange(url)}
-                    onRemove={() => field.onChange("")}
-                  />
-                </FormControl>
-              </FormItem>
-            )}
-          />
           <div className="grid grid-cols-3 gap-8">
             <FormField
               control={form.control}
-              name="label"
+              name="name"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel> Label </FormLabel>
+                  <FormLabel> Name </FormLabel>
                   <FormControl>
-                    <Input disabled={loading} placeholder="Billboard label" {...field} />
+                    <Input disabled={loading} placeholder="Category name" {...field} />
                   </FormControl>
                 </FormItem>
               )}
@@ -154,4 +136,4 @@ const BillboardForm: React.FC<BillboardFormProps> = ({
   )
 }
 
-export default BillboardForm
+export default CategoryForm
