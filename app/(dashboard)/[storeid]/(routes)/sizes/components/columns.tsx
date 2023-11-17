c36dc82051e8d564132ad0e5ed75e7669289aae8
RicardoCS18 <ricardog.carreras@gmail.com>
2023-11-17 13:57:32 -0500

    size added

--- app/(dashboard)/[storeid]/(routes)/sizes/components/columns.tsx
+++ app/(dashboard)/[storeid]/(routes)/sizes/components/columns.tsx
@@ -4,23 +4,29 @@ import { ColumnDef } from "@tanstack/react-table"
 import { CellAction } from "./cell-action";
 
 
-export type BillboardColumn = {
-    id: string;
-    label: string;
-    createdAt: string;
+export type SizeColumn = {
+  id: string;
+  name: string;
+  value: string;
+  createdAt: string;
 }
 
-export const columns: ColumnDef<BillboardColumn>[] = [
-    {
-        accessorKey: "label",
-        header: "Label",
-    },
-    {
-        accessorKey: "createdAt",
-        header: "Date",
-    },
-    {
-        id: "actions",
-        cell: ({ row }) => <CellAction data={row.original} />
-    }
+export const columns: ColumnDef<SizeColumn>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "value",
+    header: "Value",
+  }
+  ,
+  {
+    accessorKey: "createdAt",
+    header: "Date",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original} />
+  }
 ]
